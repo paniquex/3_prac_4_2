72f9113221c81c7c52d3d5094c471d8b46f5044f
paniquex <paniquexx@gmail.com>
2018-12-10 22:24:02 +0300

    realized semaphor-synchro without error check and shared memory

--- client.c
+++ client.c
@@ -3,19 +3,38 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
 
 int server_finish = 0;
 
-int main(int agrc, char *argv[]) {
+int main(int agrc, char *argv[]) { //0 - resource // 1 - server
 	key_t key = ftok("resource", 'r');
-	int semid_resource = semget(key, 1, IPC_CREAT | 0666);
-	int semid_server = semget(key, 1, IPC_CREAT | 0666);
-	int shmid = shmget(key , sizeof(int), IPC_CREAT | 0666);
+	int semid_resource = semget(key, 2, 0);
+	if (semid_resource == -1) {
+		printf("error\n");
+	}
+	semctl(semid_resource, 0, SETVAL, (int) 1);
+	semctl(semid_resource, 1, SETVAL, (int) 0);
+
+	int shmid = shmget(key , sizeof(int), 0);
 	char *shmaddr = shmat(shmid, NULL, 0);
+
+
 	struct sembuf sops;
 	sops.sem_num = 0;
 	sops.sem_flg = 0;
+	sops.sem_op = -1;
+	semop(semid_resource, &sops, 1);
 
+	if (fork() == 0) {
+		execvp("ls", argv);
 	}
-
+	wait(NULL);
+	sops.sem_op = 1;
+	semop(semid_resource, &sops, 1);
+	sops.sem_num = 1;
+	semop(semid_resource, &sops, 1);
+	return 0;
 }
