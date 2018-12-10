6f1279eaad55467401375c9282425459670f8f88
paniquex <paniquexx@gmail.com>
2018-12-10 22:23:45 +0300

    realized semaphor-synchro without error check and shared memory

--- main.c
+++ main.c
@@ -3,21 +3,47 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
 
 int server_finish = 0;
 
-int main(int agrc, char *argv[]) {
+void
+sigterm_handler(int sig);
+
+int main(int agrc, char *argv[]) { //0 - resource // 1 - server
+	signal(SIGINT, sigterm_handler);
 	key_t key = ftok("resource", 'r');
-	key_t key_sem_server = ftok("server", 'r');
-	int semid_resourse = semget(key, 1, IPC_CREAT | 0666);
-	int semid_server = semget(key_sem_server, 1, IPC_CREAT | 0666);
+	int semid_resource = semget(key, 2, IPC_CREAT | 0666);
+	semctl(semid_resource, 0, SETVAL, (int) 1);
+	semctl(semid_resource, 1, SETVAL, (int) 0);
+
 	int shmid = shmget(key , sizeof(int), IPC_CREAT | 0666);
 	char *shmaddr = shmat(shmid, NULL, 0);
+	
+
+
 	struct sembuf sops;
 	sops.sem_num = 0;
 	sops.sem_flg = 0;
-	sops.sem_op = -1;
 	while (server_finish != 1) {
-		semop(semid_resourse, &sops, 1);
+		sops.sem_op = -1;
+		sops.sem_num = 1;
+		semop(semid_resource, &sops, 1);
+		sops.sem_num = 0;
+		semop(semid_resource, &sops, 1);
+		printf("server\n");
+		sops.sem_op = 1;
+		semop(semid_resource, &sops, 1);
 	}
+	semctl(semid_resource, 0, IPC_RMID, NULL);
+	semctl(semid_resource, 1, IPC_RMID, NULL);
+	shmdt(shmaddr);
+	return 0;
+}
+
+
+void
+sigterm_handler(int sig) {
+	server_finish = 1;
 }
