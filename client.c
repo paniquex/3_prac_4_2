f8e7f9955d7bd58577748142dfce79ed3737efdd
paniquex <paniquexx@gmail.com>
2018-12-11 23:03:24 +0300

    all working correct, but without error-check, need to fix it

--- client.c
+++ client.c
@@ -7,44 +7,66 @@
 #include <unistd.h>
 #include <wait.h>
 
-int server_finish = 0;
+const int SHARED_MEMORY_SIZE = 500;
 
 int main(int agrc, char *argv[]) { //0 - resource // 1 - server
 	key_t key = ftok("resource", 'r');
-	int semid_resource = semget(key, 2, 0);
+	int semid_resource = semget(key, 3, 0);
 	if (semid_resource == -1) {
 		printf("error\n");
+		return 1;
 	}
-	semctl(semid_resource, 0, SETVAL, (int) 1);
-	semctl(semid_resource, 1, SETVAL, (int) 0);
+//	semctl(semid_resource, 0, SETVAL, (int) 1);
+//	semctl(semid_resource, 1, SETVAL, (int) 0);
 
-	int shmid = shmget(key , sizeof(int), 0);
+	int shmid = shmget(key , SHARED_MEMORY_SIZE, 0);
 	char *shmaddr = shmat(shmid, NULL, 0);
 
 
 	struct sembuf sops;
-	sops.sem_num = 0;
-	sops.sem_flg = 0;
 	sops.sem_op = -1;
+	sops.sem_num = 2;
+	printf("User waiting for server\n");
 	semop(semid_resource, &sops, 1);
+	printf("User waiting for resources\n");
 
+	sops.sem_op = -1;
+	sops.sem_num = 0;
+	semop(semid_resource, &sops, 1);
+	printf("User working with resources\n");
 	if (fork() == 0) {
 		int fd[2];
 		pipe(fd);
 		if (fork() == 0) {
 			dup2(fd[1], 1);
+			close(fd[1]);
+			close(fd[0]);
 			execvp("ls", argv);
 			return 0;
 		}
-		dup2(fd[0], 0);
-		while (read(fd[0], shmaddr, sizeof(char)));
+		close(fd[1]);
+		int i = 0;
+		int read_error;
+		while ((read_error = read(fd[0], shmaddr + i, sizeof(char))) != 0) {
+			if (read_error != -1) {
+				i++;
+			}
+			if (i >= SHARED_MEMORY_SIZE) {
+				break;
+			}
+		}
+		char terminate_symbol = '\0';
+		*(shmaddr + i) = terminate_symbol;
+		close(fd[0]);
 		wait(NULL);
 	}
 	wait(NULL);
 
 	sops.sem_op = 1;
 	semop(semid_resource, &sops, 1);
+	printf("User has ended work with resources\n");
 	sops.sem_num = 1;
 	semop(semid_resource, &sops, 1);
+	printf("User is off\n");
 	return 0;
 }
