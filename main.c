0052b3176f957b6523c8dae4a371428d16d8ec10
paniquex <paniquexx@gmail.com>
2018-12-13 17:46:25 +0300

    reconstructed semaphore model, but server executes two time in a row

--- main.c
+++ main.c
@@ -17,13 +17,14 @@ char *shmaddr;
 void
 sigterm_handler(int sig);
 
-int main(int agrc, char *argv[]) { //0 - resource // 1 - server_sleep // 2 - server_ready_to_process_user
+int main(int agrc, char *argv[]) { //0 - resource // 1 - server_sleep // 2 - server_count // 3 - client_count
 	signal(SIGINT, sigterm_handler);
 	key_t key = ftok("resource", 'r');
-	semid_resource = semget(key, 3, IPC_CREAT | 0666);
+	semid_resource = semget(key, 4, IPC_CREAT | 0666);
 	semctl(semid_resource, 0, SETVAL, (int) 1);
 	semctl(semid_resource, 1, SETVAL, (int) 0);
-	semctl(semid_resource, 2, SETVAL, (int) 0);
+	semctl(semid_resource, 2, SETVAL, (int) 1);
+	semctl(semid_resource, 3, SETVAL, (int) 1);
 
 
 	shmid = shmget(key, SHARED_MEMORY_SIZE, IPC_CREAT | 0666);
@@ -32,22 +33,17 @@ int main(int agrc, char *argv[]) { //0 - resource // 1 - server_sleep // 2 - ser
 	struct sembuf sops;
 	sops.sem_flg = 0;
 	while (server_finish != 1) {
-		sops.sem_op = 1;
-		sops.sem_num = 2;
-		semop(semid_resource, &sops, 1);
-		printf("Server is ready to next client\n");
-		printf("Server is sleeping...\n");
-		sops.sem_op = -2;
-		sops.sem_num = 1;
-		semop(semid_resource, &sops, 1);
-		printf("Server is on... and waiting for resources\n");
-
 		sops.sem_op = -1;
-		sops.sem_num = 0;
+		sops.sem_num = 2;
+		printf("-----Server is waiting end of another SERVER work...-----\n");
 		semop(semid_resource, &sops, 1);
-		printf("Server working with resources\n");
-		sleep(4);
-
+		sops.sem_num = 1;
+		printf("-----Server is waiting end of USER work-----\n");
+		semop(semid_resource, &sops, 1);
+		sops.sem_num = 0;
+		printf("-----Server is waiting for release of shared resources-----\n");
+		semop(semid_resource, &sops, 1);
+		printf("-----Server starts working with shared resources-----\n");
 		//work with shared memory
 		int i = 0;
 		char symbol;
@@ -64,12 +60,13 @@ int main(int agrc, char *argv[]) { //0 - resource // 1 - server_sleep // 2 - ser
 				break;
 			}
 		}
-		printf("server\n");
-
 		sops.sem_op = 1;
 		sops.sem_num = 0;
 		semop(semid_resource, &sops, 1);
-		printf("Server has ended work with resources\n\n");
+		printf("-----Server has ended work with shared resources-----\n");
+		sops.sem_num = 2;
+		semop(semid_resource, &sops, 1);
+		printf("-----Server has ended full work\n\n");
 	}
 }
 
